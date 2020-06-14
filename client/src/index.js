de0b4e4d36767be72da153b94a0dd756d543fc3b
Johnny Kang <junyoung@my.yorku.ca>
2020-06-13 23:55:00 -0400

    Commit

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-//initiate react application
-//Everything created from react is going to be inside here
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
